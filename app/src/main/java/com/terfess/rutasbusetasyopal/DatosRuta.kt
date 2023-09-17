db8582b02c3966f3795c2f883dfb8c73daed5b68
FraySantiago <santiagofontalvo13@gmail.com>
2023-09-17 10:14:18 -0500

    se borraron comentarios

--- app/src/main/java/com/terfess/rutasbusetasyopal/DatosRuta.kt
+++ app/src/main/java/com/terfess/rutasbusetasyopal/DatosRuta.kt
@@ -4,9 +4,4 @@ data class DatosRuta(
     val numRuta: Int,
     val sitios: String,
     val precio: String
-    /*val numRuta: Task<DataSnapshot>,
-    val sitios: Task<DataSnapshot>,
-    val precio: Task<DataSnapshot>
-
-    val numRuta: DatabaseReference = FirebaseDatabase.getInstance("https://rutasbusetas-default-rtdb.firebaseio.com/").getReference("features/0/geometry/ruta2/numRuta")*/
 )
