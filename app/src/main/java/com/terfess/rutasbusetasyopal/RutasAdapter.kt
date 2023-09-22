1ec6579b12a1fd20feb66f98055e07878761e89d
FraySantiago <santiagofontalvo13@gmail.com>
2023-09-22 10:38:58 -0500

    change diseño de contenedores rutas y fix themes.xml

--- app/src/main/java/com/terfess/rutasbusetasyopal/RutasAdapter.kt
+++ app/src/main/java/com/terfess/rutasbusetasyopal/RutasAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class RutasAdapter(private var listado: List<DatosRuta>) : RecyclerView.Adapter<RutasHolder>() {
-    var color:String = "#DC7633"
+    var color:String = "#000000"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RutasHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return RutasHolder(layoutInflater.inflate(R.layout.activity_seccion, parent, false))
