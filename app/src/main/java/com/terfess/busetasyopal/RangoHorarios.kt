9dd06f5441028680bc5bce5d2afa7eacb50f1551
FraySantiago <santiagofontalvo13@gmail.com>
2023-10-24 17:06:11 -0500

    add cambia color de titulo dia de semana de acuerdo al dia actual

--- app/src/main/java/com/terfess/busetasyopal/RangoHorarios.kt
+++ app/src/main/java/com/terfess/busetasyopal/RangoHorarios.kt
@@ -1,18 +1,17 @@
 package com.terfess.busetasyopal
 
-import java.text.SimpleDateFormat
 import java.util.*
 import java.time.LocalTime
 
 class RangoHorarios {
-    fun BusetaEnServicio(horaInicio: String, horaFinal: String): Int {
+    fun busetaEnServicio(horaInicio: String, horaFinal: String): Int {
         //obtener hora actual
         val horaActual = LocalTime.now()
-
         val horaInicioTipoLT = LocalTime.parse(horaInicio)
         val horaFinalTipoLT = LocalTime.parse(horaFinal)
 
         //comprueba si la hora actual está dentro del rango
+        // LT es LocalTime
         return if (horaActual.isAfter(horaInicioTipoLT) && horaActual.isBefore(
                 horaFinalTipoLT
             )
@@ -21,5 +20,10 @@ class RangoHorarios {
         } else {
             0
         }
+    }
+
+    fun busetaEnDia(): Int {
+        val calendario = Calendar.getInstance()
+        return calendario.get(Calendar.DAY_OF_WEEK)
     }
 }
--- app/src/main/java/com/terfess/busetasyopal/RutasHolder.kt
+++ app/src/main/java/com/terfess/busetasyopal/RutasHolder.kt
@@ -12,16 +12,21 @@ import com.terfess.busetasyopal.databinding.ActivitySeccionBinding
 class RutasHolder(vista: View) : RecyclerView.ViewHolder(vista) {
     private val binding = ActivitySeccionBinding.bind(vista)
     private val caja = vista.context as Activity
-    private var rutaEnServicioLV = "#000000"
-    private var rutaEnServicioSab = "#000000"
-    private var rutaEnServicioDom = "#000000"
+    private var rutaEnServicioLV = "#000000" //negro
+    private var rutaEnServicioSab = "#000000" //negro
+    private var rutaEnServicioDom = "#000000" //negro
+    private var rutaEnDia = "#191163"
 
-    fun mostrar(dato: DatosRuta, color: String) {
+    fun mostrar(dato: DatosRuta, colorDia: String) {
+        val color = colorDia
+        var colorLunVier = "#524e4e"
+        var colorSab = "#524e4e"
+        var colorDom = "#524e4e"
 
-        val ruta = "Ruta\n"+dato.numRuta
-        val sitios = "<font color='$color' style='text-align:center'><b>Lugares Relevantes</b></font> <br> ${dato.sitios}"
-
-        val datos2 :DatoHorario = ListaHorarios.busetaHorario[dato.numRuta]
+        val ruta = "Ruta\n" + dato.numRuta
+        val sitios =
+            "<font color='$color' style='text-align:center'><b>Lugares Relevantes</b></font> <br> ${dato.sitios}"
+        val datos2: DatoHorario = ListaHorarios.busetaHorario[dato.numRuta]
         val claseRango = RangoHorarios()
         val horaInicioLV = datos2.horaInicioLunesViernes
         val horaFinalLV = datos2.horaFinalLunesViernes
@@ -29,38 +34,66 @@ class RutasHolder(vista: View) : RecyclerView.ViewHolder(vista) {
         val horaFinalSab = datos2.horaFinalSab
         val horaInicioDom = datos2.horaInicioDom
         val horaFinalDom = datos2.horaFinalDom
-        val resultado1 = claseRango.BusetaEnServicio(horaInicioLV, horaFinalLV)
-        val resultado2 = claseRango.BusetaEnServicio(horaInicioSab, horaFinalSab)
-        val resultado3 = claseRango.BusetaEnServicio(horaInicioDom, horaFinalDom)
+        val resultado1 = claseRango.busetaEnServicio(horaInicioLV, horaFinalLV)
+        val resultado2 = claseRango.busetaEnServicio(horaInicioSab, horaFinalSab)
+        val resultado3 = claseRango.busetaEnServicio(horaInicioDom, horaFinalDom)
 
-        when (resultado1){
+        //obtener dia actual y actuar en consecuencia
+        when (RangoHorarios().busetaEnDia()) {
+            //aqui se cambia el color del texto seccion horarios segun el dia de la semana, lunes a viernes, sabados, domingos
+            2, 3, 4, 5, 6 -> { //entre semana
+                colorLunVier = rutaEnDia
+                colorSab = color
+                colorDom = color
+            }
+
+            7 -> { //sabado
+                colorLunVier = color
+                colorSab = rutaEnDia
+                colorDom = color
+            }
+
+            1 -> { //domingo
+                colorLunVier = color
+                colorSab = color
+                colorDom = rutaEnDia
+            }
+        }
+
+        when (resultado1) {
             0 -> {
                 this.rutaEnServicioLV = "#8a0322" //rojo oscuro
             }
+
             1 -> {
                 this.rutaEnServicioLV = "#006b15" //verde oscuro
             }
         }
-        when (resultado2){
+        when (resultado2) {
             0 -> {
                 this.rutaEnServicioSab = "#8a0322" //rojo oscuro
             }
+
             1 -> {
                 this.rutaEnServicioSab = "#006b15" //verde oscuro
             }
         }
-        when (resultado3){
+        when (resultado3) {
             0 -> {
                 this.rutaEnServicioDom = "#8a0322" //rojo oscuro
             }
+
             1 -> {
                 this.rutaEnServicioDom = "#006b15" //verde oscuro
             }
         }
 
-        val horLunVie = "<font color='$color'><b>Lunes a Viernes</b></font> <br> <font color='$rutaEnServicioLV'>${dato.horLunVie}<br>${dato.frecLunVie}</font>"
-        val horSab = "<font color='$color'><b>Sabados</b></font> <br> <font color='$rutaEnServicioSab'>${dato.horSab}<br>${dato.frecSab}</font>"
-        val horDom = "<font color='$color'><b>Domingos y Festivos</b></font> <br> <font color='$rutaEnServicioDom'>${dato.horDomFest}<br>${dato.frecDomFest}</font>"
+        val horLunVie =
+            "<font color='$colorLunVier'><b>Lunes a Viernes</b></font> <br> <font color='$rutaEnServicioLV'>${dato.horLunVie}<br>${dato.frecLunVie}</font>"
+        val horSab =
+            "<font color='$colorSab'><b>Sabados</b></font> <br> <font color='$rutaEnServicioSab'>${dato.horSab}<br>${dato.frecSab}</font>"
+        val horDom =
+            "<font color='$colorDom'><b>Domingos y Festivos</b></font> <br> <font color='$rutaEnServicioDom'>${dato.horDomFest}<br>${dato.frecDomFest}</font>"
 
         binding.numRuta.text = ruta
         binding.sitios.text = Html.fromHtml(sitios, FROM_HTML_MODE_LEGACY)
