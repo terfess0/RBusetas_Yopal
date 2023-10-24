f53cbaf2c8f3da6c898941b0725c870d98fe2c1a
FraySantiago <santiagofontalvo13@gmail.com>
2023-10-24 17:15:48 -0500

    change ahora solo cambia colores en horarios que son del dia actual

--- app/src/main/java/com/terfess/busetasyopal/RutasHolder.kt
+++ app/src/main/java/com/terfess/busetasyopal/RutasHolder.kt
@@ -15,17 +15,16 @@ class RutasHolder(vista: View) : RecyclerView.ViewHolder(vista) {
     private var rutaEnServicioLV = "#000000" //negro
     private var rutaEnServicioSab = "#000000" //negro
     private var rutaEnServicioDom = "#000000" //negro
-    private var rutaEnDia = "#191163"
+    private var rutaEnDia = "#221785"
 
     fun mostrar(dato: DatosRuta, colorDia: String) {
-        val color = colorDia
         var colorLunVier = "#524e4e"
         var colorSab = "#524e4e"
         var colorDom = "#524e4e"
 
         val ruta = "Ruta\n" + dato.numRuta
         val sitios =
-            "<font color='$color' style='text-align:center'><b>Lugares Relevantes</b></font> <br> ${dato.sitios}"
+            "<font color='$colorDia' style='text-align:center'><b>Lugares Relevantes</b></font> <br> ${dato.sitios}"
         val datos2: DatoHorario = ListaHorarios.busetaHorario[dato.numRuta]
         val claseRango = RangoHorarios()
         val horaInicioLV = datos2.horaInicioLunesViernes
@@ -37,54 +36,61 @@ class RutasHolder(vista: View) : RecyclerView.ViewHolder(vista) {
         val resultado1 = claseRango.busetaEnServicio(horaInicioLV, horaFinalLV)
         val resultado2 = claseRango.busetaEnServicio(horaInicioSab, horaFinalSab)
         val resultado3 = claseRango.busetaEnServicio(horaInicioDom, horaFinalDom)
+        fun darColorDisponibilidadLunVie(){
+            when (resultado1) {
+                0 -> {
+                    this.rutaEnServicioLV = "#8a0322" //rojo oscuro
+                }
 
+                1 -> {
+                    this.rutaEnServicioLV = "#006b15" //verde oscuro
+                }
+            }
+        }
+        fun darColorDisponibilidadSab(){
+            when (resultado2) {
+                0 -> {
+                    this.rutaEnServicioSab = "#8a0322" //rojo oscuro
+                }
+
+                1 -> {
+                    this.rutaEnServicioSab = "#006b15" //verde oscuro
+                }
+            }
+        }
+        fun darColorDisponibilidadDom(){
+            when (resultado3) {
+                0 -> {
+                    this.rutaEnServicioDom = "#8a0322" //rojo oscuro
+                }
+
+                1 -> {
+                    this.rutaEnServicioDom = "#006b15" //verde oscuro
+                }
+            }
+        }
         //obtener dia actual y actuar en consecuencia
         when (RangoHorarios().busetaEnDia()) {
             //aqui se cambia el color del texto seccion horarios segun el dia de la semana, lunes a viernes, sabados, domingos
             2, 3, 4, 5, 6 -> { //entre semana
                 colorLunVier = rutaEnDia
-                colorSab = color
-                colorDom = color
+                colorSab = colorDia
+                colorDom = colorDia
+                darColorDisponibilidadLunVie()
             }
 
             7 -> { //sabado
-                colorLunVier = color
+                colorLunVier = colorDia
                 colorSab = rutaEnDia
-                colorDom = color
+                colorDom = colorDia
+                darColorDisponibilidadSab()
             }
 
             1 -> { //domingo
-                colorLunVier = color
-                colorSab = color
+                colorLunVier = colorDia
+                colorSab = colorDia
                 colorDom = rutaEnDia
-            }
-        }
-
-        when (resultado1) {
-            0 -> {
-                this.rutaEnServicioLV = "#8a0322" //rojo oscuro
-            }
-
-            1 -> {
-                this.rutaEnServicioLV = "#006b15" //verde oscuro
-            }
-        }
-        when (resultado2) {
-            0 -> {
-                this.rutaEnServicioSab = "#8a0322" //rojo oscuro
-            }
-
-            1 -> {
-                this.rutaEnServicioSab = "#006b15" //verde oscuro
-            }
-        }
-        when (resultado3) {
-            0 -> {
-                this.rutaEnServicioDom = "#8a0322" //rojo oscuro
-            }
-
-            1 -> {
-                this.rutaEnServicioDom = "#006b15" //verde oscuro
+                darColorDisponibilidadDom()
             }
         }
 
@@ -100,7 +106,6 @@ class RutasHolder(vista: View) : RecyclerView.ViewHolder(vista) {
         binding.horarioLV.text = Html.fromHtml(horLunVie, FROM_HTML_MODE_LEGACY)
         binding.horarioS.text = Html.fromHtml(horSab, FROM_HTML_MODE_LEGACY)
         binding.horarioDF.text = Html.fromHtml(horDom, FROM_HTML_MODE_LEGACY)
-
 
         binding.contenedor.setOnClickListener {
             val selector = dato.numRuta
