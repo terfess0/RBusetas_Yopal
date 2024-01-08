8f0f490aa19316c1cf9925f29f411641081327a6
FraySantiago <santiagofontalvo13@gmail.com>
2024-01-08 12:07:47 -0500

    update: version del codigo para publicar en play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.terfess.busetasyopal"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
