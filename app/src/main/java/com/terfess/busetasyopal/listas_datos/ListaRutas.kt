f33f49480c485fb3ef65a020e38a49b95899fa2e
FraySantiago <santiagofontalvo13@gmail.com>
2024-01-10 16:12:23 -0500

    Chenge: reducidos sitios innecesarios, sobre todo calles y carreras

--- app/src/main/java/com/terfess/busetasyopal/listas_datos/ListaRutas.kt
+++ app/src/main/java/com/terfess/busetasyopal/listas_datos/ListaRutas.kt
@@ -109,7 +109,9 @@ class ListaRutas {
             ),*/
             DatosPrimariosRuta(
                 2,
-                "-La Bendición, Llano lindo, San Marcos, Avenida Primera, El Silencio, Calle 40 #1, 7 de Agosto, Calle 40 #5, SuperMio Calle 40, Estadio Santiago Atalayas, Unitropico Transversal 15, Sena, Coliseo 20 de Julio, Carrera 18, Sisben, Bomberos, Cusiana Gas, La Ñapa, Transversal 18 #14, Estancia, Carrera 19, Centro, Famedic Nueva EPS, La Herradura, El Brasero, I.E Manuela Beltran, Calle 26, Carrera 18, Calle 35, Conjunto Casa Blanca 1, Glorieta Calle 40 #5, Avenida Primera, Llano Lindo, La Bendicion.",
+                "La Bendición, Llano lindo, San Marcos, Avenida Primera, El Silencio, 7 de Agosto, Estadio Santiago Atalayas, " +
+                        "Unitropico, Sena, 20 de Julio, Bomberos, Cusiana Gas, Estancia, Centro, Famedic, La Herradura, " +
+                        "I.E Manuela Beltran, Conjunto Casa Blanca 1, Llano Lindo, La Bendicion.",
                 "Cada 8 minutos",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
@@ -119,8 +121,9 @@ class ListaRutas {
             ),
             DatosPrimariosRuta(
                 3,
-                "Llano Lindo, Cancha chavinave, Barrio San Jorge, Central de Abastos, Glorieta Calle 30, Almacenes Paraiso, \n" +
-                        "Carrera 14, Bomberos, Creadimotos Honda, Banco Mundo Mujer, Alkosto, Carrera 29, Centro Comercial Hobo, Unicentro, Clinica Casanare, Famedic, Bancolombia, Gobernacion, La Estancia, LingoYes, Clinica Simalink, Calle 24, La Carpa-Petrobras, Carrera 5, Molinos Arroz Casanare, El Progreso 2, Llano Lindo.",
+                "Llano Lindo, Barrio San Jorge, Central de Abastos, Almacenes Paraiso, Bomberos, Creadimotos Honda, " +
+                        "Banco Mundo Mujer, Alkosto, CC Hobo, Unicentro, Clinica Casanare, Famedic, Gobernación, La Estancia, " +
+                        "Clinica Simalink, La Carpa, Molinos Arroz Casanare, El Progreso 2, Llano Lindo.",
                 "Cada 8 minutos",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
@@ -135,7 +138,9 @@ class ListaRutas {
             ),*/
             DatosPrimariosRuta(
                 6,
-                "Carrera 13, Calle 11, Barrio Bellavista, Acentamiento La Victoria, Barrio Cataluña, Calle 42, Estadio Santiago Atalayas, Calle 40, SuperMio, Transversal 7, Super Metro, Calle 30, Colegio Paraiso, Carrera 12, Fitness People Gym, Calle 24, Bomberos, Yamaha, Carrera 20, La Herradura, Calle 15, Carrera 24, Calle 7, Parque Santander, La Estancia, Carrera 19, Centro, La Herradura, Calle 24, Carrera 12b, Coliseo Casiquiare, Calle 30, Transversal 7, Calle 40, Barrio Cataluña, Calle 44, Sede Sitcol, Barrio Sueño Real",
+                "Barrio Bellavista, Acentamiento La Victoria, Barrio Cataluña, Estadio Santiago Atalayas, SuperMio, Super Metro, " +
+                        "Colegio Paraiso, Bomberos, La Herradura, Parque Santander, La Estancia, Carrera 19, Centro, La Herradura, " +
+                        "Coliseo Casiquiare, Barrio Cataluña, Sede Sitcol, Barrio Sueño Real",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
                 "Cada 12 minutos",
@@ -146,7 +151,9 @@ class ListaRutas {
             ),
             DatosPrimariosRuta(
                 7,
-                "Villa David, Las Americas, Serpet JR Y CIA S.A.S, Casa de la Justicia, Hospital Juan Luis Londoño, Calle 24, Alkosto, SAO Olimpica, Terminal, Carrera 23, Calle 7, Gobernación de Casanare, La Estancia, Centro, La Herradura, Calle 24, I.E Carlos Lleras Restrepo, Colegio Tereza de Calcuta, Villa David",
+                "Villa David, Las Americas, Casa de la Justicia, Hospital Juan Luis Londoño, " +
+                        "Alkosto, SAO Olimpica, Terminal, Gobernación, La Estancia, Centro, " +
+                        "La Herradura, I.E Carlos Lleras Restrepo, Colegio Tereza de Calcuta, Villa David",
                 "Cada 8 minutos",
                 "Cada 8 minutos",
                 "Cada 12 minutos",
@@ -157,7 +164,9 @@ class ListaRutas {
             ),
             DatosPrimariosRuta(
                 8,
-                "Coliseo Villa Lucia, Parque Llano Vargas, Carrera 43, Calle 30, Hospital Juan Luis Londoño, Calle 24, Olimpica, Terminal, Bomberos, Carrera 16, Parque El Resurgimiento, La Herradura, La Estancia, I.E Luis Hernandez Vargas, Hospital HORO, Gobernacion de Casanare, Carrera 23, Super La Placita Campesina, I.E Carlos Lleras Restrepo, Calle 30",
+                "Coliseo Villa Lucia, Parque Llano Vargas, Hospital Juan Luis Londoño, " +
+                        "Olimpica, Terminal, Bomberos, Parque El Resurgimiento, La Herradura, La Estancia, " +
+                        "I.E Luis Hernandez Vargas, HORO, Gobernacion, La Placita Campesina, I.E Carlos Lleras Restrepo",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
                 "Cada 12 minutos",
@@ -167,7 +176,10 @@ class ListaRutas {
             ),
             DatosPrimariosRuta(
                 9,
-                "El Silencio, Calle 40 #1, Barrio 7 de Agosto, EDS Terpel, Megacolegio Progreso II, Barrio Bellavista, Colegio Tecnico Ambiental, Hospital Central de Yopal, Calle 30, Exito Yopal, Alkosto, CC El Hobo, CC Unicentro, Calle 9, La Estancia, Hospital HORO, I.E Luis Hernandez Vargas, Gobernacion de Casanare, Calle 10, Clinica Casanare, La Placita Campesina, Alkosto, Glorieta Calle 40, Ocobos",
+                "El Silencio, Barrio 7 de Agosto, Megacolegio Progreso II, Barrio Bellavista, " +
+                        "Colegio Tecnico Ambiental, Hospital Central de Yopal, Exito, Alkosto, CC Hobo, " +
+                        "CC Unicentro, La Estancia, HORO, I.E Luis Hernandez Vargas, Gobernacion, " +
+                        "Clinica Casanare, La Placita Campesina, Alkosto, Glorieta Calle 40, Ocobos",
                 "Cada 7 minutos",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
@@ -177,7 +189,9 @@ class ListaRutas {
             ),
             DatosPrimariosRuta(
                 10,
-                "Las Americas, Parex Resources, ITEY, Gimnasio BHG, Parque Intra, Carrera 20, La Herradura, Calle 12, Clinica Casanare, Calle 7, Gobernacion, La Estancia, Centro, Carrera 20, Sena, Unitropico, Colegio Tereza de Calcuta, Villa David",
+                "Las Americas, ITEY, Parque Intra, La Herradura, " +
+                        "Clinica Casanare, Gobernacion, La Estancia, Centro, Sena, Unitropico, " +
+                        "Colegio Tereza de Calcuta, Villa David",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
                 "Cada 12 minutos",
@@ -187,7 +201,9 @@ class ListaRutas {
             ),
             DatosPrimariosRuta(
                 11,
-                "Las Americas, Parque Barrio El Oasis, Carrera 23, Terminal de Transporte, Carrera 26, Mascotilandia, Clinica Casanare, RapiRoy, Calle 9, Hospital HORO, Patinodromo, I.E Luis Hernandez Vargas, La Estancia, Calle 10, Insulab Diagnostic, Carrera 25, Canchas Multigol, Las Americas",
+                "Getsemani, Villa David, Las Americas, Parque Barrio El Oasis, Terminal de Transporte, Mascotilandia, " +
+                        "Clinica Casanare, RapiRoy, Hospital HORO, Patinodromo, I.E Luis Hernandez Vargas, La Estancia, " +
+                        "Insulab Diagnostic, Canchas Multigol, Las Americas, Cementerio de Yopal, Getsemani",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
                 "Cada 12 minutos",
@@ -197,7 +213,10 @@ class ListaRutas {
             ),
             /*DatosPrimariosRuta(12, "Sin sitios---","---","---","---","---","---","---"),*/
             DatosPrimariosRuta(
-                13, "La Bendicion, Llano Lindo, Glorieta Calle 50, Central de Abastos, Glorieta calle 30, Servientrega Principal, Calle 24, Hotel Capitolio del Llano, Bomberos Yopal, Yamaha, Carrera 20, La Herradura, Calle 9, La Estancia, Centro, Brasero, Calle 24, Museo Centro Historico, EDS La Carpa, Molinos Arroz Casanare, Postobon S.A, Transversal 5, Llano Lindo, La Bendición.zqx2w+ ",
+                13, "La Bendicion, Llano Lindo, Central de Abastos, " +
+                        "Servientrega Principal, Hotel Capitolio del Llano, Bomberos Yopal, " +
+                        "La Herradura, La Estancia, Centro, Brasero, Museo Centro Historico, EDS La Carpa, " +
+                        "Molinos Arroz Casanare, Postobon S.A, Llano Lindo, La Bendición",
                 "Cada 8 minutos",
                 "Cada 8 minutos",
                 "Cada 10 minutos",
