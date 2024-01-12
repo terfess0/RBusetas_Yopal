4746c2e25e71e0dc50984314c98926cf91d949ca
FraySantiago <santiagofontalvo13@gmail.com>
2024-01-12 17:24:41 -0500

    update: codigo de version para publicar 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.terfess.busetasyopal"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
