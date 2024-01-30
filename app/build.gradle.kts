067922875071d9ae2892c1fb2125966769e03084
FraySantiago <santiagofontalvo13@gmail.com>
2024-01-30 14:12:18 -0500

    update: update version de codigo para actualizar en google play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.terfess.busetasyopal"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.2"
+        versionCode = 8
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
