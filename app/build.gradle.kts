26f17904efcd0e4e811a06587f514110d9e0a5bb
FraySantiago <santiagofontalvo13@gmail.com>
2024-02-06 22:56:13 -0500

    update - 6 de febrero 2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.terfess.busetasyopal"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.0.6"
+        versionCode = 15
+        versionName = "1.0.7-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
