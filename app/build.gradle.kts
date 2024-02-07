bdca1257dc433718dc5590cb176b1a94c7038753
FraySantiago <santiagofontalvo13@gmail.com>
2024-02-06 20:23:29 -0500

    ids anuncios retirados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 13
-        versionName = "1.0.7"
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
