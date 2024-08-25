1d009ecb9b597a9e913ca48e2756d57796198c7f
terfess0 <santiagofontalvo13@gmail.com>
2024-08-25 08:23:15 -0500

    firebase errors enum

---
+++ app/src/main/java/com/terfess/busetasyopal/enums/FirebaseErrors.kt
@@ -0,0 +1,10 @@
+package com.terfess.busetasyopal.enums
+
+enum class FirebaseErrors {
+    ERROR_CONNECTION,
+    ERROR_NOT_EXISTS,
+    ERROR_AUTH,
+    NO_EXIST_OR_DISABLED,
+    ERROR_CREDENTIAL
+
+}
