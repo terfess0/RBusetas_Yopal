d6a6cdac45a5faf246a236a1e3dd2392e0edc865
terfess0 <santiagofontalvo13@gmail.com>
2024-09-08 10:20:01 -0500

    - add: Enums for data

---
+++ app/src/main/java/com/terfess/busetasyopal/enums/RoomPeriod.kt
@@ -0,0 +1,7 @@
+package com.terfess.busetasyopal.enums
+
+enum class RoomPeriod {
+    LUN_VIE,   // Lunes a Viernes
+    SAB,       // Sábado
+    DOM_FEST   // Domingo y Festivos
+}
---
+++ app/src/main/java/com/terfess/busetasyopal/enums/RoomTypePath.kt
@@ -0,0 +1,6 @@
+package com.terfess.busetasyopal.enums
+
+enum class RoomTypePath {
+    DEPARTURE,
+    RETURN
+}
