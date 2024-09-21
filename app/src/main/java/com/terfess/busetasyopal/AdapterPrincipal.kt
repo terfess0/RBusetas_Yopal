23c740936cfaf8bf03f792462bbe14d293e5356c
terfess0 <santiagofontalvo13@gmail.com>
2024-09-21 16:55:55 -0500

    - fix warnings

--- app/src/main/java/com/terfess/busetasyopal/AdapterPrincipal.kt
+++ app/src/main/java/com/terfess/busetasyopal/AdapterPrincipal.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.terfess.busetasyopal.modelos_dato.DatosPrimariosRuta
 
 class AdapterPrincipal(private var listado: List<Int>, colorTema:String) : RecyclerView.Adapter<HolderPrincipal>() {
     var color: String = colorTema
