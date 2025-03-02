2becb43dec0866f4921287abddc810bacdb00bb1
terfess0 <santiagofontalvo13@gmail.com>
2025-03-01 20:36:41 -0500

    fix auto adjust of screen, this are a error on HyperOs2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.terfess.busetasyopal"
         minSdk = 24
         targetSdk = 35
-        versionCode = 34
-        versionName = "2.3.2"
+        versionCode = 35
+        versionName = "2.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
